a2b2d76ec685d3a910242022246b109e6b292875
Shane O'Sullivan <shaneosullivan1@gmail.com>
2021-01-29 11:39:16 +0000

    Add initial files

---
+++ pages/[...slug].tsx
@@ -0,0 +1,27 @@
+import { NextPageContext } from "next";
+
+interface Props {
+  path: string;
+  slug: Array<string>;
+}
+
+function SlugPage(props: Props) {
+  return (
+    <div>
+      <div>Hello World in the root</div>
+      <div>Path is {props.path}</div>
+      <div>
+        Slug is <pre>{JSON.stringify(props.slug, null, 2)}</pre>
+      </div>
+    </div>
+  );
+}
+
+SlugPage.getInitialProps = function (context: NextPageContext) {
+  return {
+    path: context.asPath,
+    slug: context.query.slug || [],
+  };
+};
+
+export default SlugPage;
